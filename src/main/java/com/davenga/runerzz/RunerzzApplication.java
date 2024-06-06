e0063da3c6781187f48d367e6ff16a8461e80a6c
Triplerush <fcanal@unsa.edu.pe>
2024-06-06 08:13:03 -0500

    Iniciando el proyecto ejemplo de FreeCodeCamp

---
+++ src/main/java/com/davenga/runerzz/RunerzzApplication.java
@@ -0,0 +1,13 @@
+package com.davenga.runerzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunerzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunerzzApplication.class, args);
+	}
+
+}
